dbdca58477a7e81dadbd004eefd2f90136e48a68
cooh <admin@sarinform.ru>
2017-12-01 12:33:36 +0400

    mission complit0

--- star-wars/star_wars/apis/v1/characters.py
+++ star-wars/star_wars/apis/v1/characters.py
@@ -1,6 +1,6 @@
 from arrested import Resource
 from arrested.contrib.kim_arrested import KimEndpoint
-from arrested.contrib.sql_alchemy import DBListMixin
+from arrested.contrib.sql_alchemy import DBListMixin, DBCreateMixin, DBObjectMixin
 
 from star_wars.models import db, Character
 from .mappers import CharacterMapper
@@ -8,7 +8,7 @@ from .mappers import CharacterMapper
 characters_resource = Resource('characters', __name__, url_prefix='/characters')
 
 
-class CharactersIndexEndpoint(KimEndpoint, DBListMixin):
+class CharactersIndexEndpoint(KimEndpoint, DBListMixin, DBCreateMixin):
 
 	name = 'list'
 	many = True
@@ -20,4 +20,19 @@ class CharactersIndexEndpoint(KimEndpoint, DBListMixin):
 		stmt = db.session.query(Character)
 		return stmt
 
+
+
+class CharacterObjectEndpoint(KimEndpoint, DBObjectMixin):
+
+	name = 'object'
+	url = '/<string:obj_id>'
+	mapper_class = CharacterMapper
+	model = Character
+
+	def get_query(self):
+		stmt = db.session.query(Character)
+		return stmt
+
+
 characters_resource.add_endpoint(CharactersIndexEndpoint)
+characters_resource.add_endpoint(CharacterObjectEndpoint)
