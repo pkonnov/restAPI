aab2c774b24c8d73a153a2580ae36747121dd75c
spermspase <4itonada@gmail.com>
2018-01-19 15:30:51 +0400

    test toys

--- star-wars/star_wars/models/character.py
+++ star-wars/star_wars/models/character.py
@@ -6,5 +6,7 @@ __all__= ['Character']
 class Character(BaseMixin, db.Model):
 
 	__tablename__ = 'character'
+	__tableplanet__ = 'character'
 
 	name = db.Column(db.Unicode(255), nullable=False)
+	planet = db.Column(db.Unicode(20))
